fbbae79f092fcb37796a812963f1b43c0b4c09a2
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-24 15:29:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,8 @@ def monopoly_with_fourth_tier
 	      reading_railroad: {"mortgage_value" => "$100"},
 	      pennsylvania_railroad: {"mortgage_value" => "$200"},
 	      b_and_o_railroad: {"mortgage_value" => "$400"},
-	      shortline: {"mortgage_value" => ""
+	      shortline: {"mortgage_value" => "$800"},
+	      
 	    }
 
 end
