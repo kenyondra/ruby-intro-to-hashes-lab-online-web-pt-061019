9a6b303aa5b1779ef226c4039ff6312f73ad27ef
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-24 14:29:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ end
 def monopoly_with_third_tier
 	monopoly = { 
 	  
-	  railroads#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	  railroads: #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
