8c2f89110c8499ba15174ab7bd046097c061043a
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-24 14:20:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,6 +16,8 @@ end
 
 def monopoly_with_second_tier
   monopoly = {
+    railroads
+  }
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
