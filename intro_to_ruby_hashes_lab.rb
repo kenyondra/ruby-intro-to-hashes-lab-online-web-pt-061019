c8008be70535c424a9442ef3284b59ad97eacf8c
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-24 14:28:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly = { #copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
 end
 
